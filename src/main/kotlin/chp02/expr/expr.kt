a852f805c214ac5824882791e1ba3a8fb193809e
Yun <yun@pikicast.com>
2016-11-25 15:54:26 +0900

    eval() Java 로직으로.

---
+++ src/main/kotlin/chp02/expr/expr.kt
@@ -0,0 +1,19 @@
+package chp02.expr
+
+/**
+ * Created by AidenChoi on 2016. 11. 25..
+ */
+interface Expr
+class Num(val value: Int) : Expr
+class Sum(val left: Expr, val right: Expr) : Expr
+
+fun eval(e: Expr): Int {
+    if (e is Num) {
+        val n = e as Num
+        return n.value
+    }
+    if (e is Sum) {
+        return eval(e.right) + eval(e.left)
+    }
+    throw IllegalArgumentException("Unknown expression.")
+}
---
+++ src/test/kotlin/chp02/expr/ExprTest.kt
@@ -0,0 +1,15 @@
+package chp02.expr
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+/**
+ * Created by AidenChoi on 2016. 11. 25..
+ */
+class ExprTest {
+
+    @Test
+    fun expr() {
+        assertEquals(7, eval(Sum(Sum(Num(1), Num(2)), Num(4))))
+    }
+}
