bfc60ecf0da2c7aa49d8f4fb8f65c1027628d850
Yun <yun@pikicast.com>
2016-12-02 18:12:30 +0900

    추상 클래스 및 메소드 동작 예제.

--- src/main/kotlin/chp04/Interfaces.kt
+++ src/main/kotlin/chp04/Interfaces.kt
@@ -4,7 +4,11 @@ package chp04
  * Created by AidenChoi on 2016. 12. 2..
  */
 interface Clickable {
+
+    // abstract, open
     fun click()
+
+    // open
     fun showOff() = println("I'm clickable!")
 }
 
@@ -14,6 +18,7 @@ interface Focusable {
     fun showOff() = println("I'm focusable!")
 }
 
+// final class
 class Button : Clickable, Focusable {
 
     override fun showOff() {
@@ -22,4 +27,46 @@ class Button : Clickable, Focusable {
     }
 
     override fun click() = println("I was clicked")
+}
+
+open class RichButton : Clickable {
+
+    // final function
+    fun disable() {}
+
+    // can override it in a subclass
+    open fun animate() {}
+
+    // overrides an open function and its open as well.
+    override fun click() {}
+
+    // forbid overriding
+//    final override fun click() {}
+
+}
+
+// Abstract members are always open.
+abstract class Animated {
+
+    // must be overridden
+    abstract fun animate()
+
+    // can override on subclass
+    open fun stopAnimating() {}
+
+    // final
+    fun animateTwice() {}
+}
+
+class DummyAnimated : Animated() {
+
+    override fun animate() {
+
+    }
+
+    // optional
+    override fun stopAnimating() {
+        super.stopAnimating()
+    }
+
 }
