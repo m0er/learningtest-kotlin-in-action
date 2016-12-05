d147b02389f8a8329197a2001fdec72065546142
Yun <yun@pikicast.com>
2016-12-05 12:18:31 +0900

    sealed 키워드 예제.

--- src/main/kotlin/chp02/Expr.kt
+++ src/main/kotlin/chp02/Expr.kt
@@ -3,15 +3,16 @@ package chp02
 /**
  * Created by AidenChoi on 2016. 11. 25..
  */
-interface Expr
-class Num(val value: Int) : Expr
-class Sum(val left: Expr, val right: Expr) : Expr
+sealed class Expr {
+    class Num(val value: Int) : Expr()
+    class Sum(val left: Expr, val right: Expr) : Expr()
+}
 
 fun eval(e: Expr): Int {
-    if (e is Num) {
-        val n = e as Num
+    if (e is Expr.Num) {
+        val n = e as Expr.Num
         return n.value
-    } else if (e is Sum) {
+    } else if (e is Expr.Sum) {
         return eval(e.right) + eval(e.left)
     } else {
         throw IllegalArgumentException("Unknown expression")
@@ -19,9 +20,9 @@ fun eval(e: Expr): Int {
 }
 
 fun eval2(e: Expr): Int =
-    if (e is Num) {
+    if (e is Expr.Num) {
         e.value
-    } else if (e is Sum) {
+    } else if (e is Expr.Sum) {
         eval2(e.right) + eval2(e.left)
     } else {
         throw IllegalArgumentException("Unknown expression")
@@ -29,22 +30,20 @@ fun eval2(e: Expr): Int =
 
 fun eval3(e: Expr): Int =
     when (e) {
-        is Num -> e.value
-        is Sum -> eval3(e.right) + eval3(e.left)
-        else -> throw IllegalArgumentException("Unknown expression")
+        is Expr.Num -> e.value
+        is Expr.Sum -> eval3(e.right) + eval3(e.left)
     }
 
 fun evalWithLogging(e: Expr): Int =
     when (e) {
-        is Num -> {
+        is Expr.Num -> {
             println("num: ${e.value}")
             e.value
         }
-        is Sum -> {
+        is Expr.Sum -> {
             val left = evalWithLogging(e.left)
             val right = evalWithLogging(e.right)
             println("sum $left + $right = ${left + right}")
             left + right
         }
-        else -> throw IllegalArgumentException("Unknown expression")
     }
